e2e7e7664a8c4e3a3d4bddb45e47c3ffdf273e40
Faizan Ahmed <143335334+faizanahmed01@users.noreply.github.com>
2023-09-14 18:53:02 +0500

    Add files via upload

---
+++ Exercise37/index.js
@@ -0,0 +1,10 @@
+/* Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(),
+which prints the name of each magician in the array. */
+const magicians = ['Joker', 'Pinkro', 'Neuro'];
+function show_magicians(magicians) {
+    for (const magician of magicians) {
+        console.log(magician);
+    }
+}
+show_magicians(magicians);
+export {};
