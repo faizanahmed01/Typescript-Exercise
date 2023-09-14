1d1846e1aaeebc066b220c9ce81e8cc6bd798028
Faizan Ahmed <143335334+faizanahmed01@users.noreply.github.com>
2023-09-14 17:02:33 +0500

    Add files via upload

---
+++ Exercise11/index.ts
@@ -0,0 +1,16 @@
+//Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+//Make a list that includes at least three people you’d like to invite to dinner. 
+//Then use your list to print a message to each person, inviting them to dinner.
+
+let myGuestLists:Array<string> = [
+
+    "Omer",
+    "Arsalan",
+    "Hassan",
+    
+]
+for(let myGuestList of myGuestLists)
+{
+    console.log(`${myGuestList} I am inviting you on dinner tonight`)
+}
+
