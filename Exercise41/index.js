3c8277e1eb058c82d66f0f6c34ae4bdd5f96155b
Faizan Ahmed <143335334+faizanahmed01@users.noreply.github.com>
2023-09-14 19:00:09 +0500

    Add files via upload

---
+++ Exercise41/index.js
@@ -0,0 +1,18 @@
+/* Cars: Write a function that stores information about a car in a Object. The function should always
+ receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments.
+ Call the function with the required information and two other name-value pairs, such as a color or
+ an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.*/
+function carinformation(manufacturer, model, ...properties) {
+    const car = {
+        manufacturer,
+        model,
+    };
+    for (const [key, value] of properties) {
+        car[key] = value;
+    }
+    return car;
+}
+const myCar = carinformation("Honda", "Civic", ["Color", "red"], ["speed", "300 km/hr"]);
+console.log(myCar);
+export {};
+//Output Result
