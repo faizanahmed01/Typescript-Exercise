ac60dbb049213c9e8b97805884c8e32cadb7cef7
Faizan Ahmed <143335334+faizanahmed01@users.noreply.github.com>
2023-09-14 16:53:12 +0500

    Add files via upload

---
+++ Exercise07/index.js
@@ -0,0 +1,6 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, 
+//create a message that reveals your favorite number. Print that message.
+let favoritenumber = 5;
+let messgae = `my favourite number is ${favoritenumber}`;
+console.log(messgae);
+export {};
