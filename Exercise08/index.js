633fc66381626fba0197a788c4903b4c26b1eb7f
Faizan Ahmed <143335334+faizanahmed01@users.noreply.github.com>
2023-09-14 16:55:04 +0500

    Add files via upload

---
+++ Exercise08/index.js
@@ -0,0 +1,8 @@
+//Names: Store the names of a few of your friends in a array called names. 
+//Print each person’s name by accessing each element in the list, one at a time
+let name = ["Hassan", "Ahmed", "Arsalan", "Farhan",];
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+console.log(name[3]);
+export {};
