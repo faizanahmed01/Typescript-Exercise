0de74c3a8f00ee689f745f353e8abe004478149f
Faizan Ahmed <143335334+faizanahmed01@users.noreply.github.com>
2023-09-14 16:50:33 +0500

    Add files via upload

---
+++ Exercise06/index.ts
@@ -0,0 +1,16 @@
+/*Stripping Names: Store a person’s name, and include some whitespace characters at 
+the beginning and end of the name. Make sure you use each character combination, 
+"\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. 
+Then print the name after striping the white spaces.*/
+
+
+
+
+let PersonName:string = "\t  Faian Ahmed  \n";
+console.log("Name with whitespace:",PersonName);
+
+let strippedName : string = PersonName.trim();
+console.log("Stripped name: ",strippedName)
+
+
+
