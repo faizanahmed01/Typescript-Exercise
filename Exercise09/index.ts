5323c4847617c783097c293edb17440b9382fb3d
Faizan Ahmed <143335334+faizanahmed01@users.noreply.github.com>
2023-09-14 16:57:24 +0500

    Add files via upload

---
+++ Exercise09/index.ts
@@ -0,0 +1,12 @@
+//Greetings: Start with the array you used in Exercise 11, 
+//but instead of just printing each person’s name, print a message to them. 
+//The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let names : string [] = ["Hassan","Ahmed","Arsalan","Farhan",];
+
+//Using for loop you just print a message
+
+for (let name of names)
+{
+    console.log(`Hi ${name}, I am learning typescript`);
+}
